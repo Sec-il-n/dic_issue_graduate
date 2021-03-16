3f2044c723794a22d8d0fad5233cfe0217cbbed8
secil <>
2021-03-16 19:24:13 +0900

    modify

--- spec/system/comment_spec.rb
+++ spec/system/comment_spec.rb
@@ -2,73 +2,73 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :system do
   describe 'コメント機能' do
-    # describe 'コメント投稿機能' do
-    #   context '提案詳細画面を表示した場合' do
-    #     context '提案投稿者でない場合' do
-    #       let!(:user) { create(:user, id: 1) }
-    #
-    #       let!(:user_2) { create(:user, id: 2) }
-    #       let!(:suggest) { create(:suggest, user_id: user_2.id) }
-    #       before do
-    #         suggest
-    #         login_valid_user
-    #       end
-    #       it '提案にコメントを投稿できる' do
-    #         fill_in_comment
-    #
-    #         expect(page).to have_content('aaa')
-    #       end
-    #     end
-    #     context'提案投稿者の場合' do
-    #       let!(:user) { create(:user, id: 1) }
-    #       let!(:suggests) { create_list(:suggest, 30) }
-    #       before do
-    #           login_valid_user
-    #           suggests
-    #       end
-    #       it 'コメント投稿が失敗する' do
-    #         fill_in_comment
-    #         expect(page).to have_content("#{I18n.t('.dictionary.words.cannot comment')}")
-    #       end
-    #     end
-    #   end
-    # end
-    # describe 'コメント表示機能' do
-    #   context '提案詳細画面に遷移した場合'do
-    #     context'提案に対するコメントが１件以上ある場合'do
-    #       let!(:user) { create(:user) }
-    #       let!(:user_2) { create(:user) }#Key (id)=(2) already exists.
-    #       let!(:suggest) { create(:suggest, user_id: user_2.id) }
-    #       let!(:comment) { create(:comment, suggest_id: suggest.id, user_id: user.id) }
-    #       before do
-    #         login(user)
-    #       end
-    #       it 'コメントが新着順で表示されている'do
-    #         list = comment_list
-    #         created_texts = sort_dcsc(list.last(2)).map(&:text)
-    #         show_suggest
-    #         shown_texts = all('.text').map(&:text)
-    #         created_texts.each_with_index do |text, idx|
-    #           expect(text).to eq(shown_texts[idx])
-    #         end
-    #       end
-    #       it '最初の２件までが表示されている'do
-    #         create_comment_list
-    #         show_suggest
-    #         expect(all('.comment').count).to eq(2)
-    #       end
-    #       context'「もっと見る」をクリックした場合'do
-    #         it '残りのコメントが全て表示される'do
-    #           create_comment_list
-    #           show_suggest
-    #           find('.more').click
-    #           sleep 2.0
-    #           expect(all('.comment').count).to eq(11)
-    #         end
-    #       end
-    #     end
-    #   end
-    # end
+    describe 'コメント投稿機能' do
+      context '提案詳細画面を表示した場合' do
+        context '提案投稿者でない場合' do
+          let!(:user) { create(:user, id: 1) }
+
+          let!(:user_2) { create(:user, id: 2) }
+          let!(:suggest) { create(:suggest, user_id: user_2.id) }
+          before do
+            suggest
+            login_valid_user
+          end
+          it '提案にコメントを投稿できる' do
+            fill_in_comment
+
+            expect(page).to have_content('aaa')
+          end
+        end
+        context'提案投稿者の場合' do
+          let!(:user) { create(:user, id: 1) }
+          let!(:suggests) { create_list(:suggest, 30) }
+          before do
+              login_valid_user
+              suggests
+          end
+          it 'コメント投稿が失敗する' do
+            fill_in_comment
+            expect(page).to have_content("#{I18n.t('.dictionary.words.cannot comment')}")
+          end
+        end
+      end
+    end
+    describe 'コメント表示機能' do
+      context '提案詳細画面に遷移した場合'do
+        context'提案に対するコメントが１件以上ある場合'do
+          let!(:user) { create(:user) }
+          let!(:user_2) { create(:user) }#Key (id)=(2) already exists.
+          let!(:suggest) { create(:suggest, user_id: user_2.id) }
+          let!(:comment) { create(:comment, suggest_id: suggest.id, user_id: user.id) }
+          before do
+            login(user)
+          end
+          it 'コメントが新着順で表示されている'do
+            list = comment_list
+            created_texts = sort_dcsc(list.last(2)).map(&:text)
+            show_suggest
+            shown_texts = all('.text').map(&:text)
+            created_texts.each_with_index do |text, idx|
+              expect(text).to eq(shown_texts[idx])
+            end
+          end
+          it '最初の２件までが表示されている'do
+            create_comment_list
+            show_suggest
+            expect(all('.comment').count).to eq(2)
+          end
+          context'「もっと見る」をクリックした場合'do
+            it '残りのコメントが全て表示される'do
+              create_comment_list
+              show_suggest
+              find('.more').click
+              sleep 2.0
+              expect(all('.comment').count).to eq(11)
+            end
+          end
+        end
+      end
+    end
     describe 'コメント編集機能' do
       context '提案詳細画面を表示した場合' do
         let!(:user) { create(:user) }
@@ -82,7 +82,6 @@ RSpec.describe Comment, type: :system do
             comment
           end
           it '編集リンクからコメントの編集ができる' do
-            # binding.pry
             find(:xpath, '/html/body/div[2]/div[3]/div[1]/div[1]/strong/a').click
 
             execute_script('window.scrollBy(0,10000)')
